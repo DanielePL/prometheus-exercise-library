7863db54d0a4de9966e5f6eeb4cb366cbeb8e48f
DanielePL <danielepauli@gmail.com>
2025-04-17 09:59:31 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -395,6 +395,7 @@ app.post('/api/check-duplicates', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
-});// test comment
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
